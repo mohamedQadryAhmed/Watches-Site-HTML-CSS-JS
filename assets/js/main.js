c93d08af071d2e909456ff6a67014b7fd323e87e
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-09-22 13:51:16 +0300

    design home section & featured section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,35 @@
-const menuBtn = document.querySelector('.menu-icon');
-const closeBtn = document.querySelector('.close-icon');
-const menu = document.querySelector('.nav-menu');
+const menuBtn = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector(".close-icon");
+const menu = document.querySelector(".nav-menu");
 
-if(menuBtn){
-    menuBtn.addEventListener('click', () =>{
-        menu.classList.add('show-menu')
-    })
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    menu.classList.add("show-menu");
+  });
 }
 
-if(closeBtn){
-    closeBtn.addEventListener('click', () =>{
-        menu.classList.remove('show-menu')
-    })
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+  });
 }
 
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll(".nav-link");
 
-navLinks.forEach(link => link.addEventListener('click', () =>{
-    menu.classList.remove('show-menu');
-}));
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+  })
+);
+
+// --------------- Change Header Background -------
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  if (window.scrollY >= 80) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
