119870adfab9340206cada46c9ebcb87bfb7c2c2
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-09-28 14:10:36 +0300

    design testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,13 @@ const scrollHeader = () => {
 };
 
 window.addEventListener("scroll", scrollHeader);
+
+// ----------- Testimonial SWiper --------------
+var testimonialSwiper = new Swiper(".testimonial-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
